661e6855f7916ca75b806cc6c4177cd1cb658318
NMaiztegui <naroamaiztegui@gmail.com>
2024-10-10 10:09:16 +0200

    setting

--- Scripts/Azterketa/Azterketa/settings.py
+++ Scripts/Azterketa/Azterketa/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'Azterketa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'azterketa',
+        'USER': 'root',
+        'PASSWORD': 'Admin123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+}
+
 }
 
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE =  'Europe/Madrid'
 
 USE_I18N = True
 
