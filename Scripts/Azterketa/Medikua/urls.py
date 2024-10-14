301fd34037a3efcdaf39a19abfe6ba018c5f395a
NMaiztegui <naroamaiztegui@gmail.com>
2024-10-14 09:52:54 +0200

    medikua page, pazienteak page, main oage

--- Scripts/Azterketa/Medikua/urls.py
+++ Scripts/Azterketa/Medikua/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
  path('main/', views.main_page, name='orri-nagursia'),
+ path('medikua/list/', views.medikuak_list, name='medikuak_zerrenda'),
+ path('medikua/new/', views.medikua_new, name='medikua-new'),
+ path('pazientea/list/', views.pazienteak_list, name='pazienteak-zerrenda'),
+ path('pazientea/new/' , views.pazientea_new, name='pazientea-new'),
+ path('medikua/aldatu/<int:kod_medikua>/', views.medikua_aldatu, name='medikua-aldatu' ),
+ path('medikua/list/<int:kod_medikua>/', views.medikua_ezabatu, name='medikua-ezabatu' ),
  
 ]
